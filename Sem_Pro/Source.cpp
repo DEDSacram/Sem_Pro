1cefeeddabfe72c3b1ce2ffcc5854e488de3d93b
DEDSacram <bubles013@gmail.com>
2023-02-13 21:55:07 +0100

    better_regex

--- Sem_Pro/Source.cpp
+++ Sem_Pro/Source.cpp
@@ -40,11 +40,12 @@ public:          // Access specifier
 	std::string sn;
 	std::string birthday;
 	int height;
-	int weight;
+    double weight;
 	int mother_id;
 	int father_id;
 	// int p_id,std::string p_givenName, std::string p_sn, std::string p_birthday, int p_height, int p_weight, int p_mother_id, int p_father_id
-	Person(int p_weight,int p_id,int p_father_id, std::string p_birthday, std::string p_sn, int p_height, std::string p_givenName, int p_mother_id) {
+	//Person(int p_weight,int p_id,int p_father_id, std::string p_birthday, std::string p_sn, int p_height, std::string p_givenName, int p_mother_id)
+	Person(double p_weight,int p_id,int p_father_id, std::string p_birthday, std::string p_sn, int p_height, std::string p_givenName, int p_mother_id) {
 		id = p_id;
 		givenName = p_givenName;
 		sn = p_sn;
@@ -162,31 +163,26 @@ int main() {
 	doubly_linked_list_t* lst = &list;
 	
 	std::vector<std::string> person_info = split_return(result, '\n');
-	std::regex rx("^[1-9](?:[0-9]*)?[\\.][0-9],[0-9]{5},(?:-1|[0-9]{5}),[0-9]*[-][0-9]{1,2}[-][0-9]{1,2},[A-Z][a-z]*,[0-9]{1,3},[A-Z][a-z]*,(?:-1|[0-9]{5})");
+	/*std::regex rx("(^[1-9](?:[0-9]*)?[\\.][0-9]),([0-9]{5}),((?:-1|[0-9]{5})),([0-9]*[-][0-9]{1,2}[-][0-9]{1,2}),([A-Z][a-z]*),([0-9]{1,3}),([A-Z][a-z]*),((?:-1|[0-9]{5}))");*/
+	std::regex rx("^([1-9][0-9]?\.[0-9]),([1-9][0-9]{4}),(-1|[1-9][0-9]{4}),([1-9][0-9]{3}-(?:[1-9]|1[0-2])-(?:[1-9]|[1-3][0-9])),([A-Z][a-z]*),([1-9][0-9]{0,2}),([A-Z][a-z]*),(-1|[1-9][0-9]{4})");
 
 	for (std::string i : person_info) {
 		
 		std::match_results< std::string::const_iterator > mr;
 		std::regex_search(i, mr, rx);
-		/*std::cout << i;*/
-	/*	if (mr.size()) {
-			std::cout << i;
-		}*/
-		for (std::string i : mr) {
-			std::cout << i;
+
+		if (mr.size() - 1 != 8) {
+			throw std::invalid_argument("Validation error occured");
 		}
+		//std::stoi()
+		std::cout << mr[0];
 		std::cout << std::endl;
 
-
-	/*	if (std::regex_match(i, std::regex("^[0-9]"))) {
-			std::cout << i;
-		}
-		else {
-
-		}*/
+		//double p_weight, int p_id, int p_father_id, std::string p_birthday, std::string p_sn, int p_height, std::string p_givenName, int p_mother_id
+		Person ent5 = Person((double)stod(mr[1].str()),stoi(mr[2].str()), stoi(mr[3].str()),mr[4].str(), mr[5].str(), stoi(mr[6].str()),mr[7].str(), stoi(mr[6].str()));
 	}
 
-
+	/*, mr[2], mr[3], mr[4], mr[5], mr[6], mr[7], mr[8]*/
 
 	
 	
